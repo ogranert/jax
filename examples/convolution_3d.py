114115fbd7dd05253059e4f2034070cb32350bc0
Oliver Granert <110376634+ogranert@users.noreply.github.com>
2022-08-23 13:44:06 +0200

    tidy code after running pre-commit run --all

--- examples/convolution_3d.py
+++ examples/convolution_3d.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""A basic example using JAX to smooth a 3d volume image (NIFTI image)
+"""A basic example using JAX to convolve a 3d volume image (NIFTI image)
 
-For image processing the example is based on the JAX submodule lax 
+For image processing the example is based on the JAX submodule lax
 which provides the convolution function lax.conv_general_dilated.
 Reading is done by nibable and final plotting by matplotlib.
 """
@@ -27,18 +27,17 @@ import jax.numpy as jnp
 import nibabel as nib
 
 # For plotting
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 # Read 3d image data
 img_in = nib.load("ch2bet.nii.gz")
 
 # Transfer image data to jax
-JNPIMG=jnp.array(img_in.get_fdata())
+JNPIMG = jnp.array( img_in.get_fdata() )
 
-# A 3D kernel in HWDIO layout 
+# A 3D kernel in HWDIO layout
 # (H)eight, (W)idth, (D)epth, (I)nput channel, (O)utput channel
-# Note: 
+# Note:
 # A 3D kernel is not required for this, but demonstrate the general possibility!
 kernel = jnp.array([
   [[0, 0,  0], [0,  -1,  0], [0,  0,   0]],
@@ -47,23 +46,22 @@ kernel = jnp.array([
   dtype=jnp.float32)[:, :, :, jnp.newaxis, jnp.newaxis]
 
 # Reshape input volume
-JNPIMG2=JNPIMG[jnp.newaxis, :, :, :, jnp.newaxis]
-print(JNPIMG2.shape)
+JNPIMG2 = JNPIMG[jnp.newaxis, :, :, :, jnp.newaxis]
+print( JNPIMG2.shape )
 
 dn = lax.conv_dimension_numbers(JNPIMG2.shape, kernel.shape,
                                 ('NHWDC', 'HWDIO', 'NHWDC'))
 print(dn)
 
 # Convolve data with specified 3d kernel
-out = lax.conv_general_dilated(JNPIMG2,    # lhs = NCHW image tensor
-               kernel, # rhs = OIHW conv kernel tensor
-               (1, 1, 1),  # window strides
+out = lax.conv_general_dilated(
+               JNPIMG2,   # lhs = NCHW image tensor
+               kernel,    # rhs = OIHW conv kernel tensor
+               (1, 1, 1), # window strides
                'SAME',
-               (1,1,1),  # lhs/image dilation
-               (1,1,1),  # rhs/kernel dilation
-               dn)     # dimension_numbers = lhs, rhs, out dimension permutation 
-               
-
+               (1,1,1),   # lhs/image dilation
+               (1,1,1),   # rhs/kernel dilation
+               dn)        # dimension_numbers = lhs, rhs, out dimension permutation
 
 # Plot a slice from input and result volume
 fig = plt.figure()
